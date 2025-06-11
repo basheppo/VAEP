9e27fd800d970851eafd34b787d9ab44596c0989
badr <146830709+basheppo@users.noreply.github.com>
2025-06-11 15:35:32 +0200

    Update layers.py

--- models/layers.py
+++ models/layers.py
@@ -7,23 +7,4 @@ class SamplingLayer(Layer):
         epsilon = tf.random.normal(shape=tf.shape(mean))
         return mean + tf.exp(0.5 * log_var) * epsilon
 
-class SteepSigmoid(Layer):
-    def __init__(self, k=5, **kwargs):
-        super().__init__(**kwargs)
-        self.supports_masking = True
-        self.k = k
 
-    def call(self, inputs):
-        return self.steep_sigmoid(inputs)
-
-    def steep_sigmoid(self, inputs):
-        inputs = tf.clip_by_value(inputs, -10, 10)
-        return 1 / (1+tf.exp(-self.k*inputs))
-
-    def compute_output_shape(self, input_shape):
-        return input_shape
-
-    def get_config(self):
-        config = {"k": self.k}
-        base_config = super().get_config()
-        return {**base_config, **config}
